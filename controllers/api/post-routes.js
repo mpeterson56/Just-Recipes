9d96c5b1a357773c7e73a1b07defbf727a52fbcf
Mike Peterson <mikemikemike3390@yahoo.com>
2022-06-21 20:52:54 -0700

    setup some routes in controllers file

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User,  } = require('../../models');
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
