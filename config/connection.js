5a58595fe5f2167da900afc76de5e880b5ae4f74
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-23 19:12:08 -0700

    added dotenv to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,8 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+require('dotenv').config();
+
 const connection = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL): new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
