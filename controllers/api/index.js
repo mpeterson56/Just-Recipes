1b55489f22a75cc1635da51caa50e05e8261fdac
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-28 21:08:26 -0700

    testing functionality on search

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -9,7 +9,16 @@ router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
-
+router.get('/spoon/:search', (req, res) => {
+    var spoonApiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + process.env.API_KEY + 
+    "&query=" + search +
+    "&number=6&instructionsRequired=true&addRecipeInformation=true&fillIngredients=true"
+    axios.get(spoonApiUrl).then(recipeResponse => {
+        res.json(recipeResponse)
+    }).catch(err => {
+        res.send(err);
+    })
+})
 
 
 module.exports = router;
--- public/javascript/edamam-api.js
+++ public/javascript/edamam-api.js
@@ -1,3 +1,6 @@
+// const e = require("express");
+// const { response } = require("express");
+
 var searchBtn = document.getElementById('search-btn');
 var searchText = document.getElementById("search-input");
 
@@ -6,41 +9,51 @@ var recipeIngredients = [];
 var recipeSteps = [];
 
 searchBtn.addEventListener('click', (e) => {
+    console.log('search worked');
     e.preventDefault();
     getApiData();
 });
 
 function getApiData() {
-    var spoonApiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + process.env.API_KEY + 
-    "&query=" + searchText.value +
-    "&number=6&instructionsRequired=true&addRecipeInformation=true&fillIngredients=true"
+    // var spoonApiUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + process.env.API_KEY + 
+    // "&query=" + searchText.value +
+    // "&number=6&instructionsRequired=true&addRecipeInformation=true&fillIngredients=true"
 
-    fetch(spoonApiUrl)
+    fetch(`/api/spoon/${searchText.value}`)
         .then((data) => data.json())
         .then((json) => {
+            console.log('info aquired')
             for (let i = 0; i < json.results.length; i++) {
                 // pulling each extendedIngredient[j] at each result[i] and grabbing the original which shows the measure and ingredient type
                 for (let j = 0; j < json.results.extendedIngredients.length; j++) {
                     recipeIngredients.push(json.results[i].extendedIngredients[j].original);
-                    var ingredientList = document.getElementById('ingredient-list-' + [i]);
+                    var ingredientList = document.createElement('ul' + i);
                     ingredientList.textContent = recipeIngredients[j];
                 }
 
                 // pulling each steps[p] at each result[i] and grabbing the step which shows the steps to cook the recipe in order
                 for (let p = 0; p < json.results.analyzedInstructions.steps.length; p++) {
                     recipeSteps.push(json.results[i].analyzedInstructions.steps[p].step);
-                    var cookingSteps = document.getElementById('cooking-steps-' + [i]);
+                    var cookingSteps = document.createElement('ol' + i);
                     cookingSteps.textContent = recipeSteps[p];
                 }
 
                 // pushes recipe titles to an array and fills a correlated text area with the title at each index
                 recipeTitles.push(json.results[i].title);
-                var title = document.getElementById('title-' + [i]);
+                var title = document.createElement('h2' + i);
                 title.textContent = recipeTitles[i];
                 // gets the image, gets the element in which it will be placed, and adds it as an attribute
                 var recipeImage = json.results[i].image;
-                var image = document.getElementById('image-' + [i]);
+                var image = document.createElement('img' + i);
                 image.setAttribute("src", recipeImage);
             }
         })
+        //  fetch(spoonApiUrl).then( function(json) {
+        //     if(json.ok) {
+        //         document.location.replace('../api/post/')
+        //     } else {
+        //         alert(response.statusText)
+        //     }
+        //  })
 };
+
