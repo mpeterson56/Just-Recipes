542bf48b5e15e69edc216a00064920b0785b5814
Mike Peterson <mikemikemike3390@yahoo.com>
2022-07-10 20:19:52 -0700

    fix accept change arror

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,13 +1,9 @@
 async function logout() {
   const response = await fetch('/api/users/logout', {
     method: 'post',
-<<<<<<< HEAD
-    headers: { 'Content-Type': 'application/json' }
-=======
     headers: {
       'Content-Type': 'application/json'
     }
->>>>>>> feature/handlebars
   });
 
   if (response.ok) {
